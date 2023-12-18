3e810b7f083d710db426360338e9c2147bdb7993
Jia.Zheng <zheng.jia@archermind.com>
2023-12-18 10:46:19 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,12 +4,29 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "jazz-blog",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/jazz-blog/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "8ec4651bab9553f80c33",
+        clientSecret: "ec9c9a8fe5623cc499afdd48130e3b0a8d384979",
+        owner: "jazz",
+        repo: "jazz-blog",
+      },
+    ],
+  ],
 
   head,
   plugins,
